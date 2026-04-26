030c557375e149494905022b7ef3c6ce991cd988
La bruja programadora 🧿 <TU_CORREO_DE_GITHUB>
2026-04-25 20:33:34 -0500

    refactor: crear instancias de AcademiProfileData e implementarlas en main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,34 +4,39 @@ public class Main {
 
     public static void main(String[] args) {
 
+
         AcademicProfileService service = new AcademicProfileService();
 
-        System.out.println("\nRegister Student");
-        service.registerProfile(
-                "student",
-                "Laura Perez",
-                "CC-123456",
-                "laura.perez@school.com",
-                16,
-                "10th Grade",
-                null,
-                "Programming Basics",
-                "Morning",
-                4.9
+        AcademiProfileData student = new AcademiProfileData(
+            "student",
+            "Laura Perez",
+            "CC-123456",
+            "laura.perez@school.com",
+            16,
+            "10th Grade",
+            null,
+            "Programming Basics",
+            "Morning",
+            4.9
         );
 
-        System.out.println("\nRegister Teacher");
-        service.registerProfile(
-                "teacher",
-                "Carlos Gomez",
-                "CC-789012",
-                "carlos.gomez@school.com",
-                38,
-                null,
-                "Software Engineering",
-                "Programming Basics",
-                "Afternoon",
-                4.7
+        AcademiProfileData teacher = new AcademiProfileData(
+            "teacher",
+            "Carlos Gomez",
+            "CC-789012",
+            "carlos.gomez@school.com",
+            38,
+            null,
+            "Software Engineering",
+            "Programming Basics",
+            "Afternoon",
+            4.7
         );
+
+        System.out.println("\nRegister Student");
+        service.registerProfile(student);
+
+        System.out.println("\nRegister Teacher");
+        service.registerProfile(teacher);
     }
 }
